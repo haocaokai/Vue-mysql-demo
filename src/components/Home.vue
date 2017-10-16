42dad8c5283e03c7dfdd85bac3d7ed884277c3a3
haocaokai <307356224@qq.com>
2017-10-16 17:52:52 +0800

    2017/10/16

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,13 +1,21 @@
 <template>
   <div class="container">
     <h1>{{msg}}</h1>
+
     <el-row :gutter="24">
       <el-col :span="8" :offset="9">
         <el-input v-model="name" placeholder="姓名"></el-input>
         <el-input v-model="age" placeholder="年龄"></el-input>
-        <el-button type="primary" @click="addUser()">提交</el-button>
+        <el-button  @click="addUser()" type="primary">提交</el-button>
       </el-col>
     </el-row>
+
+    <el-table :data="tableData" stripe style="width:50%" v-loading="loading">
+      <el-table-column prop="name" label="姓名" width="100%"></el-table-column>
+      <el-table-column prop="age" label="年龄" width="100%"></el-table-column>
+    </el-table>
+
+   
   </div>
 </template>
 
@@ -22,34 +30,49 @@
         msg : 'Test',
         name : '',
         age : '',
+        tableData : [],
+        loading: false
       }
     },
     methods : {
+      // 提交数据
       addUser(){
-        
         var postdata = {
           name : this.name,
           age : this.age
         };
-        
+        var that = this;
         axios.post('http://127.0.0.1:9090/setdata',qs.stringify(postdata))
           .then(function(res){
             console.log(res.data);
-            alert('提交成功')
           }).catch(function(err){
             console.log(err)
           })
-        
-        
-        // get 请求获取数据
-        // axios.get('http://127.0.0.1:9090/getdata')
-        //  .then((res)=>{
-        //     var data = res.data;
-        //     console.log(data)
-        //     this.name = data[0].name;
-        //     this.age = data[0].age
-        //  });
+
+        this.loading = true;
+        this.name = '';
+        this.age = '';
+
+        setTimeout(function(){
+          that.$message({
+            message: '提交成功',
+            type: 'success'
+          });
+          that.loading = false;
+          that.tableData.push(postdata)
+        }, 1000)
+
+
       }
+    },
+    created : function(){
+      // 获取数据
+      axios.get('http://127.0.0.1:9090/getdata')
+       .then((res)=>{
+          var data = res.data;
+          console.log(data)
+          this.tableData = data;
+       });
     }
   }
 </script>
@@ -57,4 +80,7 @@
 <style>
   .el-input{ width: 30%; float: left; margin-right: 10px}
   button{ float: left; }
+  .el-table{  margin: 30px auto;}
+  table{ width: 100% !important; }
+  table th{ text-align: center !important; }
 </style>
